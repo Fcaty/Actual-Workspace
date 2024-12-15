042befe9802b04f05dc5f72397c36f184bf767b8
Fcaty <fcatymon@gmail.com>
2024-12-16 07:56:14 +0800

    modified:   tambak folder/testoverload.cpp

--- tambak folder/testoverload.cpp
+++ tambak folder/testoverload.cpp
@@ -1,20 +1,9 @@
 #include <stdio.h>
-#include <math.h>
-
-double function1(int test1){
-    return sqrt(test1);
-}
-
-double function1(int test1, int test2){
-    return pow(test1, test2);
-}
 
 int main(){
-    double test1 = 4, test2 = 2, result;
+    int testArray[5] {0}; 
 
-    result = function1(test1,test2);
-
-    printf("%.2lf", result);
+    printf("%d", testArray[4] + testArray[3]);
 
     return 0;
 }
